52736595ae41181f3b987df8b98c3d5e1d3f1273
Seyed Mohammad Hossein Hosseiny <49597221+smh-hosseiny@users.noreply.github.com>
2021-10-05 20:22:02 +0330

    Update GUI.m

--- GUI.m
+++ GUI.m
@@ -1,37 +1,29 @@
 function GUI
 
 addpath('files');
-% clc; clear;
+clc; clear;
 image = imread('files/TheDome.jpg');
 f = figure('Visible','off','Position',[360,500,300,230]);
 ha = axes('unit', 'normalized', 'position', [0 0 1 1]);
 imagesc(image);
 
-% global current_folder;
-% current_folder = pwd;
-
-% Construct the components.
 h1 = uicontrol('Style','pushbutton','String','Path to image',...
            'Position',[115,150,80,30], 'Callback',{@Pathbutton_Callback});
-% h2  = uicontrol('Style','text','String','Choose Method',...
-%            'Position',[315,170,100,15]);
+
+h2 = uicontrol('Style','checkbox',...
+           'String',{'Save Point cloud output'},...
+           'Position',[85,105,130,28],'Callback',{@checkbox_Callback});
        
-% h3 = uicontrol('Style','popupmenu',...
-%            'String',{'---','Fast','Slow'},...
-%            'Position',[300,140,100,25],'Callback',{@popup_menu_Callback});
-       
-h4 = uicontrol('Style','pushbutton','String','Run',...
+h3 = uicontrol('Style','pushbutton','String','Run',...
            'Position',[115,70,70,25],'Callback',{@Runbutton_Callback});
  
 
-
-align([h1,h4],'Center','None');
+align([h1,h2,h3],'Center','None');
 
 f.Units = 'normalized';
 h1.Units = 'normalized';
-% h2.Units = 'normalized';
-% h3.Units = 'normalized';
-h4.Units = 'normalized';
+h2.Units = 'normalized';
+h3.Units = 'normalized';
 f.Name = 'GUI';
 movegui(f,'center')
 f.Visible = 'on';
@@ -40,17 +32,24 @@ end
 
 function Pathbutton_Callback(source,eventdata) 
 global Image_of_dome;
-% global current_folder;
-% cd(current_folder);
+
 [file,path] = uigetfile('*.png; *.jpg; *.jpeg',...
                'Select an image');
 selectedfile = fullfile(path,file);
 Image_of_dome = imread(selectedfile);
 end
 
+function checkbox_Callback(source,eventdata)
+global point_cloud;
+point_cloud = source.Value;
+
+end
+
 function Runbutton_Callback(source,eventdata)
     global Image_of_dome;
+    global point_cloud;
+    
     close;
-    run(Image_of_dome);
+    run(Image_of_dome, point_cloud);
     GUI;
 end
