b728ce024cfa6c765a20d902065a75be80cea50b
Seyed Mohammad Hossein Hosseini <49597221+smh-hosseiny@users.noreply.github.com>
2021-11-02 21:13:44 +0330

    Update find_angle.m

--- files/find_angle.m
+++ files/find_angle.m
@@ -1,21 +1,23 @@
-function best_angle = find_angle(Image,x,y,nrow,n,refrence,s1,s2,Pbase, K, p1, dh)
+function best_angle = find_angle(Image,x,y,nrow,s1,s2,n,reference,Pbase,K,p1,dh)
 angle = [-10,0,10,20,30];
 score = [];
+idx1 = round(2*s1/10:8*s1/10);
+idx2 = round(2*s2/10:8*s2/10);
 for ang = angle
     [lb, ub] = get_range(n, ang, x,y, Pbase, K, p1, nrow, dh);
-    RGB = surface_projection(Image, n, Pbase, K, p1, lb, ub, dh, s1, s2,ang); 
-    s = ssim(RGB, refrence);
+    [RGB]  = surface_projection(Image,n,Pbase,K,p1,lb,ub,dh,s1,s2,ang); 
+    s = ssim(RGB(idx1,idx2,:), reference(idx1,idx2,:));
     score = [score, s];
 end
 
 [hs, idx] = max(score);
 ba = angle(idx);
 score2 = [];
-phase_2_angle = [ba-3, ba+3];
+phase_2_angle = [ba-4, ba-2, ba+2, ba+4];
 for ang = phase_2_angle
     [lb, ub] = get_range(n, ang, x,y, Pbase, K, p1, nrow, dh);
-    RGB = surface_projection(Image, n, Pbase, K, p1, lb, ub, dh, s1, s2,ang); 
-    s = ssim(RGB, refrence);
+    [RGB]  = surface_projection(Image,n,Pbase,K,p1,lb,ub,dh,s1,s2,ang); 
+    s = ssim(RGB(idx1,idx2,:), reference(idx1,idx2,:));
     score2 = [score2, s];
 end
 
